8c2ff56ca5aae019d2b213a2acd4bda1aa949a20
怪角 <48637361+yaoguaijiao@users.noreply.github.com>
2020-08-02 00:09:15 +0800

    Update MJS.js
    更新了背景更换代码

--- src/main/java/MJS.js
+++ src/main/java/MJS.js
@@ -1,8 +1,11 @@
 var xiaokang = new xkTool();
-xiaokang.bannerList = [
+
+// 设置随机背景的图片
+xiaokang.imgList = [
     "https://s1.ax1x.com/2020/08/01/aG58Qf.jpg",
     "https://s1.ax1x.com/2020/08/01/aG5yOU.jpg",
     "https://s1.ax1x.com/2020/08/01/aGImcV.jpg",
     "https://s1.ax1x.com/2020/08/01/aGIMBF.jpg"
 ];
-xiaokang.randomBanner(true); // true为使用滤镜，不写或者false为不使用滤镜
+// 调用随机背景
+xiaokang.randomBg();
