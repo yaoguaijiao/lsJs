1722d71fccec3898c22067a251fbf4d937d8fbf6
怪角 <48637361+yaoguaijiao@users.noreply.github.com>
2020-08-13 08:23:23 +0800

    Update blog.js

--- blog.js
+++ blog.js
@@ -2,8 +2,8 @@ $(document).ready(function (e) {
     $('.copyright').html('<i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i>');
 })
 
-/*添加图片top*/
-var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
+/*添加图片top          https://cdn.jsdelivr.net/gh/yaoguaijiao/LsImg/blogimg/20200813082053.gif  大     https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif    小    */
+var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/yaoguaijiao/LsImg/blogimg/20200813082053.gif' title='回到顶部' >";
 /*添加到返回顶部按钮下*/
 document.getElementById("go-up").innerHTML += top_up;
 
